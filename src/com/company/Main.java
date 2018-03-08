c02ede1d85ea88e1795c2297279d3061f4ccedf9
TobyBlanchard <blanchard.toby@gmail.com>
2018-03-07 23:18:37 -0500

    Created classes Cashier, Payable, PayableWithTip, and Waiter and tested them in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       Waiter w = new Waiter(20);
+        w.displayBillBeforeTip();
+        w.addTipToTotal(5);
+        System.out.println("You added a tip of $" + w.getTipAmount());
+        w.payBill(40);
+        System.out.println(w.toString());
+       Cashier c = new Cashier(10.00, 12.00);
+        System.out.println(c.toString());
+
     }
 }
